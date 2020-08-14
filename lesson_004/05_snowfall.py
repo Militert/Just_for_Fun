a5b69d7b2d0c4ff1832f825d3d320398153fc4e7
Swifty <Artur321787@gmail.com>
2020-08-15 01:40:50 +0300

    Сделал 5 задание 4 урока

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,7 +2,7 @@
 
 import simple_draw as sd
 
-
+sd.resolution = (1200, 800)
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
@@ -17,16 +17,48 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+# for _ in range(N):
+#     start_point = sd.get_point(sd.random_number(150, 1000), sd.random_number(150, 600))
+#     length = sd.random_number(20, 50)
+#     fa = sd.random_number(45, 75) / 100
+#     fb = sd.random_number(15, 40) / 100
+#     fc = sd.random_number(50, 70)
+#     sd.snowflake(center=start_point, length=length, factor_a=fa, factor_b=fb, factor_c=fc)
+
+
+def snow(x, y, lng):
+    sd.start_drawing()
+    fa = sd.random_number(45, 75) / 100
+    fb = sd.random_number(15, 40) / 100
+    fc = sd.random_number(50, 70)
+    while True:
+        sd.snowflake(center=sd.get_point(x, y), length=lng, color=sd.COLOR_WHITE,
+                     factor_a=fa, factor_b=fb, factor_c=fc)
+        sd.sleep(0.1)
+        sd.snowflake(center=sd.get_point(x, y), length=lng, color=sd.background_color,
+                     factor_a=fa, factor_b=fb, factor_c=fc)
+        y -= sd.random_number(10, 30)
+        x += sd.random_number(-40, 40)
+
+        sd.finish_drawing()
+        if y < 30:
+            sd.snowflake(center=sd.get_point(x, y), length=lng, color=sd.COLOR_WHITE,
+                         factor_a=fa, factor_b=fb, factor_c=fc)
+            break
+        sd.sleep(0.1)
+        if sd.user_want_exit():
+            break
+
+
 while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
+    length = sd.random_number(20, 50)
+    Y = 900
+    X = sd.random_number(50, 1000)
+    snow(x=X, y=Y, lng=length)
     if sd.user_want_exit():
         break
 
+sd.start_drawing()
 sd.pause()
 
 # подсказка! для ускорения отрисовки можно
@@ -43,5 +75,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-

81943fb418e12cb79a9bc8a83d9f11fa62cf7fc5
Arthur <artur321787@gmail.com>
2022-11-26 05:00:01 +0300

    Add comments and update commands list.

--- Skillfactory_Python/D_module/TeleBOT/app.py
+++ Skillfactory_Python/D_module/TeleBOT/app.py
@@ -1,23 +1,34 @@
 import telebot
+from telebot import types
 from pymorphy2 import MorphAnalyzer
 
 from time import strftime
 
-from config import currencies, TELEGRAM_TOKEN
-from extensions import APIException, CryptoConverter
+from config import *
+from extensions import *
+
+
+def create_markup(base=None):  # Создание кнопок на основе валют
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    buttons = []
+    for val in currencies.keys():
+        if val != base:
+            buttons.append(types.KeyboardButton(val.capitalize()))
+
+    markup.add(*buttons)
+    return markup
 
 
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help'])  # Команда с описанием доступных команд
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> <в какую валюту перевести> ' \
-           '<количество переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    text = '\nУвидеть список всех доступных валют: /values\nНачать конвертацию валют: /convert'
     bot.send_message(message.chat.id, f'Welcome, {message.chat.first_name}! {text}')
 
 
-@bot.message_handler(commands=['values'])
+@bot.message_handler(commands=['values'])  # Команда со списком доступных валют
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in currencies.keys():
@@ -27,27 +38,38 @@ def values(message: telebot.types.Message):
           f'({message.chat.username}) запросил список доступных валют.')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['convert'])  # Команда конвертации валют
 def convert(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать:'
+    bot.send_message(message.chat.id, text, reply_markup=create_markup())
+    bot.register_next_step_handler(message, quote_handler)  # Ждём следующего сообщения от пользователя
+
+
+def quote_handler(message: telebot.types.Message):
+    quote = message.text.strip().lower()
+    text = 'Выберите валюту, в которую конвертировать:'
+    bot.send_message(message.chat.id, text, reply_markup=create_markup(quote))  # Убираем валюту, выбранную ранее
+    bot.register_next_step_handler(message, base_handler, quote)  # Ждём следующего сообщения от пользователя
+
+
+def base_handler(message: telebot.types.Message, quote):
+    base = message.text.strip().lower()
+    text = 'Выберите количество конвертируемой валюты:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, quote, base)  # Ждём следующего сообщения от пользователя
+
+
+def amount_handler(message: telebot.types.Message, quote, base):
+    amount = message.text.strip().lower().replace(",", ".")
     try:
-        values = message.text.lower().split()
-
-        if len(values) > 3:
-            raise APIException('Слишком много аргументов.')
-        elif len(values) < 3:
-            raise APIException('Слишком мало аргументов.')
-
-        quote, base, amount = values
         total_base = CryptoConverter.get_price(quote, base, amount)
     except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
     else:
         text = f'Цена: {amount} ' \
-               f'{MorphAnalyzer().parse(quote)[0].make_agree_with_number(float(amount.replace(",", "."))).word}' \
-               f' в {MorphAnalyzer().parse(base)[0].inflect({"plur", "loct"})[0]} - {total_base}'
-        bot.send_message(message.chat.id, text)
+               f'{MorphAnalyzer().parse(quote)[0].make_agree_with_number(float(amount)).word} в ' \
+               f'{MorphAnalyzer().parse(base)[0].inflect({"plur", "loct"})[0]} - {total_base}'
+        bot.send_message(message.chat.id, text)  # Просклоняли ответное сообщение пользователю
         print(f'{strftime("%H:%M:%S")}\t{message.chat.first_name} {message.chat.last_name}'
               f'({message.chat.username}) конвертировал валюту.')
 
